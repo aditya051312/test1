e2b52c6d4ffd5910f64ef7a47d18705dfbbe69de
aditya051312 <adityakhillari1@gmail.com>
2023-09-13 13:57:23 +0530

    Adding new Dashboard.js file

---
+++ src/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div className="dashboard-page">
+      <h2>Welcome to the Dashboard</h2>
+    </div>
+  );
+}
+
+export default Dashboard;
