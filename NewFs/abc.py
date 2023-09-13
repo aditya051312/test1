41158bcb8c50c3eadf05586026d55d340fc52182
aditya051312 <adityakhillari1@gmail.com>
2023-09-13 11:02:10 +0530

    Delete NewFs directory

--- NewFs/abc.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(num1, num2):
-    """
-    This function takes two numbers as input and returns their sum.
-    """
-    return num1 + num2
-
-# Testing the function
-print(add_numbers(5, 10))
