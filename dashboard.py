c7373449008b3a799547665cc0548624c275d2bd
aditya051312 <adityakhillari1@gmail.com>
2024-01-29 15:03:42 +0530

    Delete dashboard.py

--- dashboard.py
+++
@@ -1,9 +0,0 @@
-class Dashboard:
-    def __init__(self, admin):
-        self.admin = admin
-
-    def display(self):
-        for student in self.admin.students:
-            print(f"Name: {student.name}")
-            print(f"Attendance: {student.attendance}")
-            print(f"Remarks: {student.remarks}")
